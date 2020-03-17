601e4776db717310408b3588232dd40ea0df73a7
Alexander Garcia <alexg97343@gmail.com>
2020-03-17 15:16:24 -0400

    setting up folders

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Sidebar from './components/sidebar'
+
+
+class App extends Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default App;
