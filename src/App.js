5e7415678c118a347255bc155df6076eca1651ae
Alexander Garcia <alexg97343@gmail.com>
2020-03-17 15:21:22 -0400

    added 2 more filessidebar and intro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import Sidebar from './components/sidebar'
-
+import Introduction from './components/introduction'
+import About from './components/about'
+import Projects from './components/projects'
 
 class App extends Component {
   render() {
     return (
-      
+      <div id="colorlib-page">
+        <div id="container-wrap">
+         	<Sidebar></Sidebar>
+				<div id="colorlib-main">
+					<Introduction></Introduction>
+					<About></About>
+          <Projects></Projects>
+          	</div>
+      	</div>
+      </div>
     );
   }
 }
